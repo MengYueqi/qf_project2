4de1ee57185af77afd927afeb7f8de335760d2fe
MengYueqi <mengyq2002@gmail.com>
2025-11-01 13:13:31 +0800

    add some data

--- strategy/RL/model/data_loader.py
+++ strategy/RL/model/data_loader.py
@@ -14,7 +14,7 @@ def load_real_trading_env(
     leverage_cap=1.0,
     max_episode_steps=200,
     random_start=True,
-    feature_dim=25,       # 想保留的总特征维度
+    feature_dim=30,       # 想保留的总特征维度
     random_feature_select=True,  # 是否随机选因子列
     seed=42,                # 固定随机种子方便复现
 ):
@@ -23,20 +23,34 @@ def load_real_trading_env(
 
     np.random.seed(seed)
 
-    # -------- 1. 价格 --------
+    # -------- 1. 价格和预测价格 --------
     price_dfs = []
     for t in tickers:
         fp = os.path.join(base_path, "close_price", f"{t}.csv")
         df = pd.read_csv(fp, parse_dates=["Date"])
         df = df[["Date", df.columns[1]]].rename(columns={df.columns[1]: t})
         price_dfs.append(df)
+    
+    pred_price_dfs = []
+    for t in tickers:
+        fp = os.path.join(base_path, "predict", f"{t}.csv")
+        df = pd.read_csv(fp, parse_dates=["Date"])
+        df = df[["Date", df.columns[1]]].rename(columns={df.columns[1]: t})
+        pred_price_dfs.append(df)
 
     price_df = price_dfs[0]
     for df in price_dfs[1:]:
         price_df = pd.merge(price_df, df, on="Date", how="outer")
 
+    pred_price_df = pred_price_dfs[0]
+    for df in pred_price_dfs[1:]:
+        pred_price_df = pd.merge(pred_price_df, df, on="Date", how="outer")
+
     price_df = price_df.sort_values("Date").reset_index(drop=True)
     price_df = price_df.fillna(method="ffill").fillna(method="bfill").fillna(0.0)
+
+    pred_price_df = pred_price_df.sort_values("Date").reset_index(drop=True)
+    pred_price_df = pred_price_df.fillna(method="ffill").fillna(method="bfill").fillna(0.0)
 
     # -------- 2. 收益率 --------
     rets_df = price_df.copy()
@@ -76,7 +90,7 @@ def load_real_trading_env(
     )
 
     next_prices = (
-        price_df[tickers]
+        pred_price_df[tickers]
         .iloc[1:]
         .to_numpy(dtype=np.float32)
     )
@@ -94,16 +108,18 @@ def load_real_trading_env(
     ).astype(np.float32)  # 确保 float32
 
     next_prices_noisy = next_prices * (1.0 + noise)
+    next_prices_noisy = next_prices
+    curr_prices = price_df[tickers].iloc[:-1].to_numpy(dtype=np.float32)
 
     # 拼接 [带噪声明日价格 | 其他因子特征]
     features_full = np.concatenate(
-        [next_prices_noisy, features_raw],
+        [curr_prices, next_prices_noisy, features_raw],
         axis=1
     ).astype(np.float32)  # ⭐ 关键：保证最终是 float32
 
     # -------- 5. 特征选择逻辑 --------
     if feature_dim is not None and feature_dim < features_full.shape[1]:
-        keep_base = 5  # 前五列是未来价格（带扰动）
+        keep_base = 10  # 前十列是当前价格和下日带噪价格，必须保留
         num_extra = feature_dim - keep_base
 
         all_indices = np.arange(keep_base, features_full.shape[1])
